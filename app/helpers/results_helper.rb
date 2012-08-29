328019462fc78538b3bd16068f28e785a8d1134b
Al Snow <jasnow@email.com>
2012-08-28 21:22:57 -0400

    fixed results_helper cane error

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,8 +1,6 @@
 module ResultsHelper
-  def display_response(r_set,question)
-    sets = r_set.responses.select do |r|
-      r.question.display_order == question.display_order
-    end
+  def display_response(r_set, question)
+    sets = get_sets(r_set)
 
     if sets.size == 0
       return_text = "-"
@@ -19,6 +17,12 @@ module ResultsHelper
       return_text = txt
     end
     return_text
+  end
+
+  def get_sets(r_set)
+    r_set.responses.select do |r|
+      r.question.display_order == question.display_order
+    end
   end
 
   def show_answer(set)
